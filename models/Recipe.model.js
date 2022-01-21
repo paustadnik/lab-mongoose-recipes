dafa50f2bf41d1059874d2cf7aedf542dbeafed6
Paulina Stadnik <paulina.stadnik@rover.com>
2022-01-21 17:51:00 +0100

    iteration 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,39 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'Ultra Pro Chef']
+  },
+  ingredients: {
+    type: [String],
+  },
+  cuising: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'dessert', 'other']
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg"'
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: String,
+  created: {
+    type: Date,
+    defult: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
